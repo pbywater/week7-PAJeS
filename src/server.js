86ffef696f80af3b40209f297a66e27229e62fcc
Joey Scott <joannascott29@hotmail.co.uk>
2017-04-05 15:13:32 +0100

    Add routes, views, register modules Add initial hbs files so that server will run Relates #3

--- src/server.js
+++ src/server.js
@@ -13,6 +13,27 @@ server.connection({
   host,
 });
 
+server.register([inert, vision], (err) => {
+  if (err) throw err;
+
+  server.views({
+    engines: {hbs: handlebars},
+    path: 'views',
+    layout: 'default',
+    layoutPath: 'views/layout',
+    partialsPath: 'views/partials',
+    helpersPath: 'views/helpers'
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, reply) => {
+      reply.view('index');
+    }
+  });
+});
+
 server.start((err) => {
   if (err) throw err;
   console.log(`Server is running on ${server.info.uri}`);
