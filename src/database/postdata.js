24c616ac206c0e1fc42f824d3c62437167381a26
Joey Scott <joannascott29@hotmail.co.uk>
2017-04-07 12:44:26 +0100

    Change query so that credentials.username is available and can be used to insert into blogposts Relates #51

--- src/database/postdata.js
+++ src/database/postdata.js
@@ -3,16 +3,24 @@ const db_connection = require('../../database/db_connection.js');
 const postData = {};
 
 // TODO Change the last value from 4 to a variable that references username
-postData.insertIntoDatabase = (reqPayload, callback) => {
-  const query = `INSERT INTO blogposts ( title,body,username)
-    VALUES ('${reqPayload.title}','${reqPayload.content}',4)
-  ;`;
-  db_connection.query(query, (err, dbResponse) => {
+postData.insertIntoDatabase = (reqPayload, credentials, callback) => {
+  // const id = `SELECT users.id FROM users WHERE users.username = ${credentials.username};`
+  db_connection.query(`SELECT users.id FROM users WHERE users.username = '${credentials.username}'`, (err, dbResponse) => {
     if (err) {
       return callback(err);
     }
-    callback(null, dbResponse);
-  });
+    const id = dbResponse.rows[0].id;
+    const query = `INSERT INTO blogposts(title, body, username)
+      VALUES ('${reqPayload.title}','${reqPayload.content}',${id})`;
+    db_connection.query(query, (err, dbResponse) => {
+      if (err) {
+        return callback(err);
+      }
+      callback(null, dbResponse);
+    });
+  })
+
+
 };
 
 module.exports = postData;
